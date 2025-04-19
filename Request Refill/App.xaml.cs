bf301a1272168a4e9d3baa24383775d470006b23
Никита <hukuta47@yandex.ru>
2025-04-19 22:03:30 +0900

    Подправил пару нюансов

--- Request Refill/App.xaml.cs
+++ Request Refill/App.xaml.cs
@@ -3,16 +3,12 @@ using System.Windows;
 using System.Windows.Forms;
 using System.Drawing;
 using System.IO;
-using System.Reflection;
 using Request_Refill.Windows;
-using MessageBox = System.Windows.MessageBox;
 using System.Windows.Resources;
+using System.Windows.Media.Imaging;
 
 namespace Request_Refill
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
     public partial class App : System.Windows.Application
     {
         private NotifyIcon notifyIcon;
@@ -21,45 +17,74 @@ namespace Request_Refill
         {
             base.OnStartup(e);
             this.ShutdownMode = ShutdownMode.OnExplicitShutdown;
-
+            
 
             // Создаем иконку в трее
             notifyIcon = new NotifyIcon();
-
-            Uri iconUri = new Uri("pack://application:,,,/Resources/Printer.ico", UriKind.Absolute);
-            StreamResourceInfo sri = System.Windows.Application.GetResourceStream(iconUri);
-            notifyIcon.Icon = new Icon(sri.Stream);
-
+            notifyIcon.MouseClick += NotifyIcon_MouseClick;
+            notifyIcon.Icon = SelectIcon("16\\Printer W.ico");
             notifyIcon.Visible = true;
 
             // Создаем контекстное меню
             ContextMenuStrip contextMenu = new ContextMenuStrip();
-            contextMenu.Items.Add("Создание заявки на заправку", null, CreateRequestFill_Click);
-            contextMenu.Items.Add("Настройки", null, Settings_Click);
-            contextMenu.Items.Add("Закрыть", null, Close_Click);
+            contextMenu.Items.Add("Создание заявки на заправку", SelectImage("16/File B.png"), CreateRequestFill_Click);
+            contextMenu.Items.Add("Настройки", SelectImage("16/Settings B.png"), Settings_Click);
+            contextMenu.Items.Add("Закрыть", SelectImage("16/Shutdown B.png"), Close_Click);
 
             // Привязываем меню к иконке
             notifyIcon.ContextMenuStrip = contextMenu;
         }
-        private void CreateRequestFill_Click(object sender, EventArgs e)
-        {
-            new WindowCreateRequestRefill().ShowDialog();
-        }
 
-        private void Settings_Click(object sender, EventArgs e)
-        {
-            new WindowSettings().ShowDialog();
-        }
 
-        private void Close_Click(object sender, EventArgs e)
+        void NotifyIcon_MouseClick(object sender, MouseEventArgs e)
         {
-            Shutdown();
+            notifyIcon.ContextMenuStrip.Show(Cursor.Position);
         }
+        private void CreateRequestFill_Click(object sender, EventArgs e) => new WindowCreateRequestRefill().ShowDialog();
+        private void Settings_Click(object sender, EventArgs e) => new WindowSettings().ShowDialog();
+        private void Close_Click(object sender, EventArgs e) => Shutdown();
 
         protected override void OnExit(ExitEventArgs e)
         {
             notifyIcon.Dispose(); // Очищаем ресурсы
             base.OnExit(e);
+        }
+
+
+        Icon SelectIcon(string FileName)
+        {
+            Uri iconUri = new Uri($"pack://application:,,,/Resources/{FileName}", UriKind.Absolute);
+            StreamResourceInfo sri = System.Windows.Application.GetResourceStream(iconUri);
+
+            return new Icon(sri.Stream);
+        }
+
+        Image SelectImage(string fileName)
+        {
+            Uri uri = new Uri($"pack://application:,,,/Resources/{fileName}", UriKind.Absolute);
+            StreamResourceInfo sri = System.Windows.Application.GetResourceStream(uri);
+
+            if (sri == null)
+                throw new FileNotFoundException($"Image resource not found: {fileName}");
+
+            using (var bmpStream = sri.Stream)
+            {
+                BitmapImage bmpImage = new BitmapImage();
+                bmpImage.BeginInit();
+                bmpImage.StreamSource = bmpStream;
+                bmpImage.CacheOption = BitmapCacheOption.OnLoad;
+                bmpImage.EndInit();
+                bmpImage.Freeze();
+
+                using (var ms = new MemoryStream())
+                {
+                    BitmapEncoder encoder = new PngBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(bmpImage));
+                    encoder.Save(ms);
+
+                    return Image.FromStream(new MemoryStream(ms.ToArray()));
+                }
+            }
         }
 
     }
