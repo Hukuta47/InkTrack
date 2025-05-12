45bd43da84c926c8f29804a92ca548221a7a6c22
Никита <hukuta47@yandex.ru>
2025-05-12 15:35:41 +0900

    Решил сделать мастер настройки программы при первом запуске

---
+++ Request Refill/Pages/SetupWizardSettings_Page2.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+
+namespace Request_Refill.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для FirstSetupSettings_Page2.xaml
+    /// </summary>
+    public partial class SetupWizardSettings_Page2 : Page
+    {
+        public SetupWizardSettings_Page2()
+        {
+            InitializeComponent();
+        }
+    }
+}
