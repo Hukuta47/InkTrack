9191649abe01ecc45a9259f5995913349c9e1faf
Никита Зелтынь <hukuta47@yandex.ru>
2025-06-25 16:38:58 +0900

    Обновил структуру создания замены картриджа

--- InkTrack Report/Windows/ReplaceCartridge.xaml.cs
+++ InkTrack Report/Windows/ReplaceCartridge.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using InkTrack_Report.Database;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -9,14 +10,18 @@ namespace InkTrack_Report.Windows
 {
     public partial class ReplaceCartridge : Window
     {
-        int SelectedPrinterID = Properties.Settings.Default.SelectedPrinterID;
+        Printer SelectedPrinter = App.entities.Printer.First(p => p.PrinterID == Properties.Settings.Default.SelectedPrinterID);
 
         List<Cartridge> ListCartritgesForReplace;
         public ReplaceCartridge()
         {
+            ListCartritgesForReplace = App.entities.Cartridge.Where(c => c.CartridgeModel.Printer.Any(p => p.PrinterID == SelectedPrinter.PrinterID) && c.StatusID == 2).ToList();
+
             InitializeComponent();
 
             Listbox_CauseReplaceCartridge.ItemsSource = App.entities.ReasonForReplacement.ToList();
+            Combobox_CartridgeOnReplace.ItemsSource = ListCartritgesForReplace;
+            Combobox_WhoReplaced.ItemsSource = App.entities.EmployeeLIT.ToList();
         }
         private void PanelDrag_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -28,6 +33,22 @@ namespace InkTrack_Report.Windows
 
         private void Save_Click(object sender, RoutedEventArgs e)
         {
+            Cartridge SelectedCartridge = Combobox_CartridgeOnReplace.SelectedItem as Cartridge;
+
+            CartridgeReplacement_Log cartridgeReplacement_Log = new CartridgeReplacement_Log()
+            {
+                OldCartridgeID = SelectedPrinter.Cartridge.CartridgeID,
+                NewCartridgeID = (int)Combobox_CartridgeOnReplace.SelectedValue,
+                PrinterID = SelectedPrinter.PrinterID,
+                EmployeeLitID = (int)Combobox_WhoReplaced.SelectedValue,
+                ReasonID = (int)Listbox_CauseReplaceCartridge.SelectedValue,
+                Comment = Textbox_Description.Text
+            };
+            App.entities.CartridgeReplacement_Log.Add(cartridgeReplacement_Log);
+            SelectedPrinter.Cartridge = SelectedCartridge;
+            SelectedCartridge.StatusID = 3;
+
+            App.entities.SaveChanges();
 
         }
     }
