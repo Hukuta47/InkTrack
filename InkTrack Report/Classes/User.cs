a435812cf6e4edfa98ea08afd4a49eedc616d02b
Никита <hukuta47@yandex.ru>
2025-07-05 19:51:47 +0900

    Начал реализацию авто подбора пользователя по имени учетной записи

---
+++ InkTrack Report/Classes/User.cs
@@ -0,0 +1,26 @@
+﻿using InkTrack_Report.Windows.Dialog;
+using System;
+using System.Linq;
+
+namespace InkTrack_Report.Classes
+{
+    public class User
+    {
+        static public int GetID()
+        {
+            if (Environment.UserName.ToLower() == "сотрудник")
+            {
+                var dialog = new GetEmployeeIdDialog();
+                if (dialog.ShowDialog() == true)
+                {
+                    return dialog.EmployeeId;
+                }
+            }
+            else
+            {
+                return App.entities.Employee.FirstOrDefault(e => e.DomainName == Environment.UserName.ToLower()).EmployeeID;
+            }
+            return 0;
+        }
+    }
+}
---
+++ InkTrack Report/Windows/Dialog/GetEmployeeIdDialog.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace InkTrack_Report.Windows.Dialog
+{
+    public partial class GetEmployeeIdDialog : Window
+    {
+        public int EmployeeId;
+        public GetEmployeeIdDialog()
+        {
+            InitializeComponent();
+            Combobox_SelectEmployee.ItemsSource = App.entities.Employee.ToList();
+        }
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            EmployeeId = (int)Combobox_SelectEmployee.SelectedValue;
+            DialogResult = true;
+        }
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
